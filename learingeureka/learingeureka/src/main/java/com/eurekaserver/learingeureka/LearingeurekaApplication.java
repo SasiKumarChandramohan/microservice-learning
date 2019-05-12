073a63b40bb276dd71f32391d5707b11bf15b4aa
SasiKumarChandramohan <50535271+SasiKumarChandramohan@users.noreply.github.com>
2019-05-12 07:59:22 +0530

    Add files via upload
    Eureka Client - Registry

---
+++ learingeureka/learingeureka/src/main/java/com/eurekaserver/learingeureka/LearingeurekaApplication.java
@@ -0,0 +1,15 @@
+package com.eurekaserver.learingeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LearingeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearingeurekaApplication.class, args);
+	}
+
+}
